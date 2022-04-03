664476031dd745dffe31c057b1c73bedef284f77
Yossi Maatook <yossix07@gmail.com>
2022-04-03 19:44:38 +0300

    Update LogIn.js

--- src/LogIn.js
+++ src/LogIn.js
@@ -3,20 +3,7 @@
 function LogIn() {
   return (
     <div>
-      <form action="">
-        <label for="username" >Username</label>
-        <input id="username" type="text" required></input>
-        <br></br>
-        <label for="password">Password</label>
-        <input id="password" type="password" required></input>
-        <br></br>
-        <label for="repeatPassword">Verify Password</label>
-        <input id="repeatPassword" type="password" required></input>
-        <br></br>
-        <label for="displayName">Display Name</label>
-        <input id="displayName" type="text" required></input>
-        <br></br>
-      </form>
+      Login Form Will Be Here!
     </div>
   );
 }
