7947598f2193f1e141028571061f313218dfc31f
Yossi Maatook <yossix07@gmail.com>
2022-04-03 19:40:11 +0300

    add LogIn component and lanch it on start

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import LogIn from './LogIn';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LogIn />
   </React.StrictMode>,
   document.getElementById('root')
 );
