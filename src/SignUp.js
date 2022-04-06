b797fb71c9249bd7331456c1f8c42505bc840aea
Yossi Maatook <yossix07@gmail.com>
2022-04-06 23:07:31 +0300

    merging branch to master

--- src/SignUp.js
+++ src/SignUp.js
@@ -5,44 +5,72 @@ import "./SignUp.css";
 
 function SignUp() {
   const [file, setFile] = useState();
-    function fileUpload(e) {
-        console.log(e.target.files);
-        setFile(URL.createObjectURL(e.target.files[0]));
-    }
+  function fileUpload(e) {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
 
   return (
     <div id="page">
-      <form action="" id="sign-up-form" className="p-3 mb-2 bg-dark text-dark bg-opacity-75 border border-2 rounded border-danger mx-auto">
-        <h1>Create Acount</h1>
-        <div>
-          <label for="username" className="m-2">Username</label>
-          <input id="username" type="text" placeholder="Enter Username" autoFocus required></input>
+      <form action="" id="sign-up-form">
+        <div class="container">
+          <div class="row align-items-start row justify-content-start">
+            <div class="row ">
+              <h1>Create Account</h1>
+            </div>
+            <div class="row ">
+              <div class="col">
+                <label for="username" className="m-4">Username</label>
+              </div>
+              <div class="col">
+                <input id="username" className="m-4" type="text" placeholder="Enter Username" autoFocus required></input>
+              </div>
+            </div>
+            <br></br>
+            <div class="row">
+              <div class="col">
+                <label for="password" className="m-4">Password</label>
+              </div>
+              <div class="col">
+                <input id="password" className="m-4" type="password" placeholder="Enter Password" required></input>
+              </div>
+            </div>
+            <br></br>
+            <div class="row">
+              <div class="col">
+                <label for="repeatPassword" className="m-4">Verify Password</label>
+              </div>
+              <div class="col">
+                <input id="repeatPassword" className="m-4" type="password" placeholder="Enter Password Again" required></input>
+              </div>
+            </div>
+            <br></br>
+            <div class="row">
+              <div class="col">
+                <label for="displayName" className="m-4">Display Name</label>
+              </div>
+              <div class="col">
+                <input id="displayName" className="m-4" type="text" placeholder="Enter Nickname" required></input>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <label for="profilePic" className="m-4">Profile Picture</label>
+              </div>
+              <div class="col">
+                <input id="profilePic" className="m-4" type="file" onChange={fileUpload} required></input>
+              </div>
+              <div class="col">
+                <img id="profilePicImg" src={file}></img>
+              </div>
+            </div>
+            <br></br>
+            <span> Already signed-up?</span> <a href='/'>click here</a> <span> to log-in!</span>
+            <br></br>
+            <br></br>
+            <button type="button" class="btn btn-danger">Submit</button>
+          </div>
         </div>
-        <br></br>
-        <div>
-        <label for="password" className="m-2">Password</label>
-        <input id="password" type="password" placeholder="Enter Password" required></input>
-        </div>
-        <br></br>
-        <div>
-        <label for="repeatPassword" className="m-2">Verify Password</label>
-        <input id="repeatPassword" type="password" placeholder="Enter Password Again" required></input>
-        </div>
-        <br></br>
-        <div>
-        <label for="displayName" className="m-2">Display Name</label>
-        <input id="displayName" type="text" placeholder="Enter Nickname" required></input>
-        </div>
-        <div>
-        <label for="profilePic" className="m-2">Profile Picture</label>
-        <input id="profilePic" type="file" onChange={fileUpload} required></input>
-        <img id="profilePicImg" src={file} />
-        </div>
-        <br></br>
-        <span> Already signed-up?</span> <a href='/'>click here</a> <span> to log-in!</span>
-        <br></br>
-        <br></br>
-        <button type="button" class="btn btn-danger">Submit</button>
       </form>
     </div>
   );
