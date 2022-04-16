a0f0b567fa3178e947fe68d2834b7f2eb129fc3b
Yossi Maatook <yossix07@gmail.com>
2022-04-17 01:21:21 +0300

    can send videos

--- src/chatPage/chatWindow/ReceiverMessage.js
+++ src/chatPage/chatWindow/ReceiverMessage.js
@@ -1,15 +1,48 @@
-function Message(props) {
-    return (
-        <div className="d-flex justify-content-start mb-4">
-            <div className="img_cont_msg">
-                <img src={props.img} className="rounded-circle user_img_msg" />
+import "../Chat.css"
+
+function ReciverMessage(props) {
+
+    switch (props.type) {
+        case "text": return (
+            <div className="d-flex justify-content-start mb-4">
+                <div className="img_cont_msg">
+                    <img src={props.img} className="rounded-circle user_img_msg" />
+                </div>
+                <div className="msg_cotainer">
+                    {props.msgText}
+                    <span className="msg_time">{props.msgTime}</span>
+                </div>
             </div>
-            <div className="msg_cotainer">
-                {props.msgText}
-                <span className="msg_time">{props.msgTime}</span>
+        )
+
+            break;
+
+        case "picture": return (
+            <div className="d-flex justify-content-start mb-4">
+                <div className="img_cont_msg">
+                    <img src={props.img} className="rounded-circle user_img_msg" />
+                </div>
+                <div className="msg_cotainer">
+                    <img className="msg_cotainer_media" src={URL.createObjectURL(props.msgText)}></img>
+                    <span className="msg_time">{props.msgTime}</span>
+                </div>
             </div>
-        </div>
-    )
+        )
+
+            break;
+
+        case "video": return (
+            <div className="d-flex justify-content-start mb-4">
+                <div className="img_cont_msg">
+                    <img src={props.img} className="rounded-circle user_img_msg" />
+                </div>
+                <div className="msg_cotainer">
+                    <video controls className="msg_cotainer_media" src={URL.createObjectURL(props.msgText)}></video>
+                    <span className="msg_time">{props.msgTime}</span>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default Message;
+export default ReciverMessage;
--- src/chatPage/chatWindow/SendMsgBar.js
+++ src/chatPage/chatWindow/SendMsgBar.js
@@ -1,9 +1,7 @@
 import { useRef, useState } from "react";
-import { addVideoMessage, addTextMessage,addPictureMessage } from "../../UsersChatDB";
-import { OverlayTrigger, Popover } from "react-bootstrap"
+import { addVideoMessage, addTextMessage, addPictureMessage } from "../../UsersChatDB";
 import $ from "jquery";
-import Recorder from "./Recorder";
-import "./SendMsgBar.css"
+import InvalidFileModal from "./InvalidFileModal";
 
 function SendMsgBar(props) {
 
@@ -19,66 +17,102 @@ function SendMsgBar(props) {
             props.refreshChat();
             textMsg.current.value = "";
         })
-        $("#msg_input").on("propertychange change keyup paste input", function () {
-            $("#".concat(btnId)).prop('disabled', $("#msg_input").val() == "");
-            console.log($("#msg_input").val());
-        })
+
     })
 
     const selectPic = () => {
         document.getElementById(btnId.concat("-img-input")).click();
     }
 
+    const validatePic = (pic) => {
+        console.log(pic)
+        var fileName = pic.name;
+        var idxDot = fileName.lastIndexOf(".") + 1;
+        var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+
+        if (extFile == "jpg" || extFile == "jpeg" || extFile == "png" || extFile == "svg") {
+            return true;
+        }
+        return false;
+    }
+
+    const [isModelOpen, setIsModelOpen] = useState(false);
+
+    const [modalText, setModalText] = useState(""); 
+
+    const showModal = () => {
+        setIsModelOpen(true);
+    };
+    const hideModal = () => {
+        setIsModelOpen(false);
+    };
+
     const sendPic = (e) => {
-        const date = new Date();
-        let time = date.getHours() + ":" + date.getMinutes();
-        addPictureMessage(props.myUser, props.username, e.target.files[0], time);
-        props.refreshChat();
+        if (validatePic(e.target.files[0])) {
+            const date = new Date();
+            let time = date.getHours() + ":" + date.getMinutes();
+            addPictureMessage(props.myUser, props.username, e.target.files[0], time);
+            props.refreshChat();
+        }
+        else {
+            setModalText("Picture format must be one of the above: jpg/jpeg/png/svg");
+            showModal();
+        }
+
+    }
+
+    const validateVid = (vid) => {
+        var fileName = vid.name;
+        var idxDot = fileName.lastIndexOf(".") + 1;
+        var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+
+        if (extFile == "mp4" || extFile == "mkv" || extFile == "avi" || extFile == "wmv" || extFile == "mov" || extFile == "flv") {
+            return true;
+        }
+        return false;
     }
 
     const selectVideo = () => {
-        document.getElementById(btnId.concat("-img-input")).click();
+        document.getElementById(btnId.concat("-vid-input")).click();
     }
 
     const sendVideo = (e) => {
-        const date = new Date();
-        let time = date.getHours() + ":" + date.getMinutes();
-        addVideoMessage(props.myUser, props.username, e.target.files[0], time);
-        props.refreshChat();
+        if (validateVid(e.target.files[0])) {
+            const date = new Date();
+            let time = date.getHours() + ":" + date.getMinutes();
+            addVideoMessage(props.myUser, props.username, e.target.files[0], time);
+            props.refreshChat();
+        } else {
+            setModalText("Video format must be one of the above: mp4/mkv/avi/wmv/mov/flv");
+            showModal();
+        }
+
     }
 
-    const popover = (
-        <Popover id="popover-basic">
-            <Popover.Header as="h1" className="popover-header">Record</Popover.Header>
-            <Popover.Body>
-                <Recorder></Recorder>
-            </Popover.Body>
-        </Popover>
-    );
-
     return (
+        <>
+        <InvalidFileModal isOpen={isModelOpen} hideModal={hideModal} text={modalText}></InvalidFileModal>
         <div className="card-footer">
             <div className="input-group">
                 <div className="input-group-append">
-                    <OverlayTrigger trigger="click" placement="top" overlay={popover}>
-                        <button type="button" className="btn btn-outline-secondary input-group-text record_btn"><i className="bi bi-mic"></i></button>
-                    </OverlayTrigger>
+                    <button type="button" className="btn btn-outline-secondary input-group-text record_btn"><i className="bi bi-mic"></i></button>
                 </div>
 
                 <div className="input-group-append">
                     <button id={btnId.concat("-img-btn")} type="button" onClick={selectPic} className="btn btn-outline-secondary input-group-text attach_img_btn" ><i className="bi bi-image"></i></button>
-                    <input id={btnId.concat("-img-input")} onChange={sendPic} type="file" accept="image/*" hidden ></input>
+                    <input id={btnId.concat("-img-input")} onChange={sendPic} type="file" accept="image/*" hidden></input>
                 </div>
                 <div className="input-group-append">
                     <button id={btnId.concat("-vid-btn")} type="button" onClick={selectVideo} className="btn btn-outline-secondary input-group-text attach_video_btn"><i className="bi bi-camera-reels"></i></button>
                     <input id={btnId.concat("-vid-input")} onChange={sendVideo} type="file" accept="video/*" hidden ></input>
                 </div>
-                <input className="type_msg form-control" ref={textMsg} placeholder="Type your message..." id="msg_input" ></input>
+                <input className="type_msg form-control" ref={textMsg} placeholder="Type your message..."></input>
                 <div className="input-group-append">
-                    <button id={btnId.concat("-msg")} type="button" className="btn btn-outline-secondary input-group-text send_btn" disabled={true} ><i className="bi bi-envelope"></i></button>
+                    <button id={btnId.concat("-msg")} type="button" className="btn btn-outline-secondary input-group-text send_btn" ><i className="bi bi-envelope"></i></button>
                 </div>
             </div>
         </div>
+        </>
     );
 }
 
