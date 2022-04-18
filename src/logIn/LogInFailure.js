c570f9f3b79c50e6e894003d5ccaa7ee6afa455e
Yossi Maatook <yossix07@gmail.com>
2022-04-18 20:41:46 +0300

    add modal styles

--- src/logIn/LogInFailure.js
+++ src/logIn/LogInFailure.js
@@ -3,12 +3,12 @@ import Modal from "react-bootstrap/Modal";
 function LogInSuccess(props) {
     return (
         <Modal show={props.isOpen} onHide={props.hideModal}>
-            <Modal.Header>
+            <Modal.Header className="bg-dark text-white">
                 <Modal.Title>Log-In Failed!</Modal.Title>
             </Modal.Header>
-            <Modal.Body>Try again please!</Modal.Body>
-            <Modal.Footer>
-                <button className="btn btn-outline-black" onClick={props.hideModal}>Try again</button>
+            <Modal.Body className="bg-dark text-white">Try again please!</Modal.Body>
+            <Modal.Footer className="bg-dark text-white">
+                <button className="btn btn-outline-light" onClick={props.hideModal}>Try again</button>
             </Modal.Footer>
         </Modal>
     );
