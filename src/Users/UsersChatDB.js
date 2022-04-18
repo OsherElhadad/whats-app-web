e48e988caa6d380dc313bbe4493a06237056d1fd
Osher Elhadad <oshereli2@walla.com>
2022-04-19 01:12:40 +0300

    =add error label to add contact, add some new fitures

--- src/Users/UsersChatDB.js
+++ src/Users/UsersChatDB.js
@@ -24,68 +24,68 @@ addUserWithEmptyChats("rol");
 addUserWithEmptyChats("ecb");
 
 // Hard-Coded Chats
-addContact("rag","lag");
-addContact("lag","rag");
-addTextMessage("rag","lag","Hello wife!", "14:00");
-addTextMessage("lag", "rag","Hello husband!", "14:01");
-addTextMessage("rag","lag","Here are some media file:", "14:02");
-addPictureMessage("rag","lag",pic1, "14:03");
-addVideoMessage("rag","lag",vid1, "14:03");
-addTextMessage("lag", "rag","Thanks :)", "14:01");
+addContact("rag", "lag");
+addContact("lag", "rag");
+addTextMessage("rag", "lag", "Hello wife!", "14:00");
+addTextMessage("lag", "rag", "Hello husband!", "14:01");
+addTextMessage("rag", "lag", "Here are some media file:", "14:02");
+addPictureMessage("rag", "lag", pic1, "14:03");
+addVideoMessage("rag", "lag", vid1, "14:03");
+addTextMessage("lag", "rag", "Thanks :)", "14:01");
 
-addContact("rag","bjo");
-addContact("bjo","rag");
-addTextMessage("rag","bjo","Hello son!", "14:31");
-addTextMessage("bjo", "rag","Hello father!", "14:50");
-addTextMessage("rag","bjo","Here are some media file:", "15:01");
-addPictureMessage("rag","bjo",pic2, "15:02");
-addVideoMessage("rag","bjo",vid2, "15:03");
-addTextMessage("bjo", "rag","thanks father :)", "15:16");
+addContact("rag", "bjo");
+addContact("bjo", "rag");
+addTextMessage("rag", "bjo", "Hello son!", "14:31");
+addTextMessage("bjo", "rag", "Hello father!", "14:50");
+addTextMessage("rag", "bjo", "Here are some media file:", "15:01");
+addPictureMessage("rag", "bjo", pic2, "15:02");
+addVideoMessage("rag", "bjo", vid2, "15:03");
+addTextMessage("bjo", "rag", "thanks father :)", "15:16");
 
-addContact("rag","rol");
-addContact("rol","rag");
-addTextMessage("rag","rol","Hello brother!", "16:50");
-addTextMessage("rol", "rag","Hello Rangar", "16:51");
-addTextMessage("rag","rol","Here are some media file:", "17:00");
-addPictureMessage("rag","rol",pic3, "17:01");
-addVideoMessage("rag","rol",vid3, "17:02");
-addTextMessage("rol", "rag","Nice", "17:03");
+addContact("rag", "rol");
+addContact("rol", "rag");
+addTextMessage("rag", "rol", "Hello brother!", "16:50");
+addTextMessage("rol", "rag", "Hello Rangar", "16:51");
+addTextMessage("rag", "rol", "Here are some media file:", "17:00");
+addPictureMessage("rag", "rol", pic3, "17:01");
+addVideoMessage("rag", "rol", vid3, "17:02");
+addTextMessage("rol", "rag", "Nice", "17:03");
 
-addContact("rag","flo");
-addContact("flo","rag");
-addTextMessage("flo","rag","Hello Rangar!", "12:00");
-addTextMessage("rag", "flo","Hello Floki", "13:51");
-addTextMessage("flo","rag","Here are some media file:", "13:55");
-addPictureMessage("flo","rag",pic4, "13:55");
-addVideoMessage("flo","rag",vid4, "13:56");
-addTextMessage("rag", "flo","God bless you", "13:56");
+addContact("rag", "flo");
+addContact("flo", "rag");
+addTextMessage("flo", "rag", "Hello Rangar!", "12:00");
+addTextMessage("rag", "flo", "Hello Floki", "13:51");
+addTextMessage("flo", "rag", "Here are some media file:", "13:55");
+addPictureMessage("flo", "rag", pic4, "13:55");
+addVideoMessage("flo", "rag", vid4, "13:56");
+addTextMessage("rag", "flo", "God bless you", "13:56");
 
-addContact("ecb","rol");
-addContact("rol","ecb");
-addTextMessage("ecb","rol","Hello Rollo!", "16:50");
-addTextMessage("rol", "ecb","Hello King Ecbert", "16:51");
-addTextMessage("ecb","rol","Here are some treats:", "17:00");
-addPictureMessage("ecb","rol",pic5, "17:01");
-addVideoMessage("ecb","rol",vid5, "17:02");
-addTextMessage("rol", "ecb","God bless you", "17:03");
+addContact("ecb", "rol");
+addContact("rol", "ecb");
+addTextMessage("ecb", "rol", "Hello Rollo!", "16:50");
+addTextMessage("rol", "ecb", "Hello King Ecbert", "16:51");
+addTextMessage("ecb", "rol", "Here are some treats:", "17:00");
+addPictureMessage("ecb", "rol", pic5, "17:01");
+addVideoMessage("ecb", "rol", vid5, "17:02");
+addTextMessage("rol", "ecb", "God bless you", "17:03");
 
-addContact("bjo","lag");
-addContact("lag","bjo");
-addTextMessage("bjo","lag","Mother!", "12:30");
-addTextMessage("lag", "bjo","Yes son?", "12:31");
-addTextMessage("bjo","lag","Here are some goodies:", "12:32");
-addPictureMessage("bjo","lag",pic6, "12:33");
-addVideoMessage("bjo","lag",vid6, "12:34");
-addTextMessage("lag", "bjo","I rasied you well", "12:34");
+addContact("bjo", "lag");
+addContact("lag", "bjo");
+addTextMessage("bjo", "lag", "Mother!", "12:30");
+addTextMessage("lag", "bjo", "Yes son?", "12:31");
+addTextMessage("bjo", "lag", "Here are some goodies:", "12:32");
+addPictureMessage("bjo", "lag", pic6, "12:33");
+addVideoMessage("bjo", "lag", vid6, "12:34");
+addTextMessage("lag", "bjo", "I rasied you well", "12:34");
 
-addContact("ecb","flo");
-addContact("flo","ecb");
-addTextMessage("ecb","flo","Hello Floki!", "10:32");
-addTextMessage("flo", "ecb","Hello Ecbert :)", "10:33");
-addTextMessage("ecb","flo","Here are some media file:", "10:40");
-addPictureMessage("ecb","flo",pic7, "10:41");
-addVideoMessage("ecb","flo",vid7, "10:41");
-addTextMessage("flo", "ecb","I thank you", "10:42");
+addContact("ecb", "flo");
+addContact("flo", "ecb");
+addTextMessage("ecb", "flo", "Hello Floki!", "10:32");
+addTextMessage("flo", "ecb", "Hello Ecbert :)", "10:33");
+addTextMessage("ecb", "flo", "Here are some media file:", "10:40");
+addPictureMessage("ecb", "flo", pic7, "10:41");
+addVideoMessage("ecb", "flo", vid7, "10:41");
+addTextMessage("flo", "ecb", "I thank you", "10:42");
 
 export function getChatMessages(user, talkWith) {
     return usersChats.get(user).find(i => i.chatWith == talkWith).messages;
@@ -135,6 +135,18 @@ function addMessage(sender, talkWith, message, msgTime, t) {
 }
 
 export function addContact(user, talkWith) {
+    var error = validateAddContact(user, talkWith);
+    if (error == "") {
+        getUserChats(user).push({
+            chatWith: talkWith,
+            messages: []
+        });
+    }
+    return error;
+}
+
+export function validateAddContact(user, talkWith) {
+    console.log(talkWith)
     if (!doesUserExist(talkWith)) {
         return "This contact does not exist";
     } else {
@@ -142,11 +154,11 @@ export function addContact(user, talkWith) {
             return "This contact is already your contact";
         }
         else {
-            getUserChats(user).push({
-                chatWith: talkWith,
-                messages: []
-            });
-            return "";
+            if (user == talkWith) {
+                return "Are you trying to add yourself?";
+            } else {
+                return "";
+            }
         }
     }
 }
@@ -189,6 +201,6 @@ export function lastMassageGeneratorTime(user, talkWith) {
     return lastMsg.time;
 }
 
-export function addUserWithEmptyChats(user){
-    usersChats.set(user,[]);
+export function addUserWithEmptyChats(user) {
+    usersChats.set(user, []);
 }
